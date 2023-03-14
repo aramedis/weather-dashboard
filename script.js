029783686c115c9856e324cd09f3fd1e18f87378
aramedis <119070223+aramedis@users.noreply.github.com>
2023-03-14 07:42:26 +0000

    added history button search functionality and cleaned up appearance

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ let dayFiveArr = [7,15,23,31,39] //5 data points I need from the 5 day forecast 
 //Search Btn to Local storage in the form of an Array (cityArr)
 $(".search-button").on("click", function() {
     event.preventDefault();
-    let city = $(".form-input").val();
+    $("#today").empty();
+    $("#forecast").empty();
+    let city = $(".weather-search").val();
     cityArr.push(city)
 
-    console.log(city, "was saved")
-    let cityBtn = $("<button>").addClass("col-1 cityBtn").text(city);
-    $(".list-group").append(cityBtn);
+    // console.log(city, "was saved")
+    let cityBtn = $("<button>").addClass("btn-secondary mt-1").text(city);
+    $("#history").append(cityBtn);
     localStorage.setItem("cityArr", JSON.stringify(cityArr));
 
     //Direct Geocoding to get Lat and Lon
@@ -69,7 +71,7 @@ function weatherApi5day(weather5dayURL) {
 
 function create5WeatherCard(response) {
     dayFiveArr.forEach(day => {
-        let time = $("<H2>").text(response.list[day].dt_txt.slice(0,10)); //slice out the last few strings (time)
+        let time = $("<H3>").text(response.list[day].dt_txt.slice(0,10)); //slice out the last few strings (time)
         let icon = $("<img>").attr("src", `https://openweathermap.org/img/wn/${response.list[day].weather[0].icon}@2x.png`).addClass("icon");
         let temp = $("<p>").text(`Temp: ${response.list[day].main.temp}°C`);
         let humidity = $("<p>").text(`Humidity: ${response.list[day].main.humidity}%`);
@@ -77,7 +79,7 @@ function create5WeatherCard(response) {
         // console.log(response.list[day].dt_txt.slice(0,10))
 
         let div = $("<div>");
-        div.addClass("card");
+        div.addClass("card text-white bg-dark m-1");
         div.append(time, icon, temp, humidity, wind);
         $("#forecast").append(div)
     });
@@ -88,11 +90,34 @@ function retrieveLocalStorage() {
     let cityArr = JSON.parse(localStorage.getItem("cityArr"));
     console.log(cityArr)
     cityArr.forEach(city => {
-        let cityBtn = $("<button>").addClass("col-1 cityBtn").text(city);
-        $(".list-group").append(cityBtn);
+        let cityBtn = $("<button>").addClass("btn-secondary mt-1").text(city);
+        $("#history").append(cityBtn);
     });
 }
 retrieveLocalStorage()
 
 
-//Direct geocoding
+//History button functionality - can be improved
+
+let historybtn = $(".btn-secondary");
+historybtn.on("click", function (event) {
+    let historyCity = event.currentTarget.outerText
+    // console.log(historyCity)
+    let geocodingUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${historyCity}&limit=1&appid=${key}`
+
+    $.ajax({
+        url: geocodingUrl,
+        method: "GET"
+      })
+        .then(function (response) {
+            // console.log(response[0].lat)
+            let weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${response[0].lat}&lon=${response[0].lon}&appid=${key}&units=metric`
+            let weather5dayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${response[0].lat}&lon=${response[0].lon}&appid=${key}&units=metric`;
+            // console.log(weatherURL)
+            weatherApiToday(weatherURL)
+            weatherApi5day(weather5dayURL)
+        })
+
+})
+
+
