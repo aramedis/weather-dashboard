572d45635b117eb6287b644fc20c32a9772883ec
aramedis <119070223+aramedis@users.noreply.github.com>
2023-03-14 04:01:26 +0000

    Save searches to local storage, and retrieve, then populate history with City Buttons

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 let key = "aa33fa3f24164a2572c3ce7f845e113a"
+let cityArr = [];
+
+
+//Search Btn to Local storage in the form of an Array (cityArr)
+$(".search-button").on("click", function() {
+    event.preventDefault();
+    let city = $(".form-input").val();
+    cityArr.push(city)
+
+    console.log(city, "was saved")
+    let cityBtn = $("<button>").addClass("col-1 cityBtn").text(city);
+    $(".list-group").append(cityBtn);
+    localStorage.setItem("cityArr", JSON.stringify(cityArr));
+});
+
+//Get history from Local storage and populate with buttons
+function retrieveLocalStorage() {
+    let cityArr = JSON.parse(localStorage.getItem("cityArr"));
+    console.log(cityArr)
+    cityArr.forEach(city => {
+        let cityBtn = $("<button>").addClass("col-1 cityBtn").text(city);
+        $(".list-group").append(cityBtn);
+    });
+}
+retrieveLocalStorage()
 
 
 //Direct geocoding
